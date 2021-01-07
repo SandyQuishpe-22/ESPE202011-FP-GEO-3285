ffad3f38db95668496916ee910623c4ad1880be1
SandyQuishpe-22 <sandydaya44@gmail.com>
2021-01-06 21:47:28 -0500

    fesd 2000

---
+++ workshop 03 operator/Feds 2000/Main.java
@@ -0,0 +1,18 @@
+public class Main{
+             public static void main (String[ ] args) {
+               Scanner scanner=new scanner (Systems.in);
+               int number=0
+
+System.out.println(“ indicate a number: ”) ;
+number = input . nextInt( ) ;
+
+if ((number % 2) == 0) {
+ System.out.println( number +  “is even”) ;
+
+}else{
+System.out.println( number +  “is odd”) ;
+
+      
+       }
+}
+     }
