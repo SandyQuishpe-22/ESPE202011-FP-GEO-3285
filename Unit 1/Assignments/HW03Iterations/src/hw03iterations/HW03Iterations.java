790497c3587b623ac13c6f8e7371d5498be3f95c
SandyQuishpe-22 <sandydaya44@mgmail.com>
2021-01-14 20:20:45 -0500

    workshops

---
+++ Unit 1/Assignments/HW03Iterations/src/hw03iterations/HW03Iterations.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hw03iterations;
+
+/**
+ *
+ * @author Usuario
+ */
+public class HW03Iterations {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+    int i;
+        i = 0;
+
+        System.out.println("==== This is a while loop from 1 to 35 ====");
+
+        while (i < 35) {
+            System.out.println("i -> " + (i + 1);
+     i++;
+     
+    }
+        
+    System.out.println("Multiplication Tables : 35");
+        int top=12;
+        int product=0;
+        int table = 35;
+        for (int j = 0 ; j <= top ; j++){
+            product = table * j;
+            System.out.println("35 * " + j + " = " + product);
+        }
+       
+    
+
+ for (char x = 'Z'; x >= 'A'; x--) {
+            for (char y = x; y >= 'A'; y--) {                                                                          
+                System.out.print(y);
+            }
+            System.out.println();
+        }
+    
