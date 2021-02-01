0eba5796301dab3600269aba87c1fb7aa41fe756
SandyQuishpe-22 <sandydaya44@gmail.com>
2021-01-31 23:22:50 -0500

    Assignments

---
+++ Assignments/Unit 2/HW08-FactorialNumber/src/hw08/factorialnumber/HW08FactorialNumber.java
@@ -0,0 +1,47 @@
+
+package hw08.factorialnumber;
+import java.util.Scanner;
+public class HW08FactorialNumber {
+public static void main(String[] args) {
+        // DONE
+        
+        int number;
+        int factorial =1;
+        int stop =15;
+        
+        
+       
+        do{
+            System.out.println("======WELCOME THE CALCULATOR TO  NUMBER FACTORIAL======");
+            System.out.println(" Enter a number ");
+            System.out.println(" Exit ");
+            
+            Scanner input = new Scanner(System.in);
+            number = input.nextInt();
+            
+            if (number <= 15){
+            System.out.println("The factorial of -->" + number);
+            for (int i = number; i > 0; i--) {
+                factorial = factorial *i;
+                
+            }
+            System.out.println("The factorial is --> " + factorial);
+            
+            }
+        }while (number <=15 );
+            
+            System.out.println("Bye Friend ");
+        
+}
+
+        
+}
+
+
+}
+        
+        
+        
+    
+    
+
